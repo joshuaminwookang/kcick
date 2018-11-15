1a8cc0d9222a67285b8e93dc5914a85b59a481b7
Joshua Minwoo Kang <jmk@Joshuas-MacBook-Air.local>
2018-11-15 18:19:45 -0500

    updated

--- q1/ProjectInterpreter.fs
+++ q1/ProjectInterpreter.fs
@@ -3,7 +3,14 @@ module ProjectInterpreter
 open Parser
 open ProjectParser
 
+let volume = [("tennis balls", 8.0); ("oranges", 7.0);("lemons", 6.0); ("Olympic-sized swimming pool", 150000000.0)] |> Map.ofList
 
+
+let eval (e: Query) (database: Map<string, string>) = 
+    let (h,o,c,comp) = 
+    match c with
+    | FitIn s -> database.[s]
+    
 (*
 let eval (q: Question) : float = 
     let head, obj, aux, cat, art, cons = q
--- q1/ProjectParser.fs
+++ q1/ProjectParser.fs
@@ -2,29 +2,47 @@ module ProjectParser
 
 open Parser
 
-type Term = 
-| Question of Term * Term * Term * Term
-| Header of string
-| Category of string
-| Object of string
+type Header = 
+| HowMany of string
+
+type Object = 
+| Main of string
 | Compare of string
 
-let volume = [("tennis balls", 8.0); ("Olympic-sized swimming pool", 150000000.0)] |> Map.ofList
+type Category = 
+| FitIn of string
 
-let is_valid(c: char)  = is_letter(c) || is_digit(c) || is_whitespace(c)
+type Query =  Header * Object * Category * Object
+
+let paux = pright pws1 (pstr "can" <|> pstr "does")
+let particle = pstr "an" <|> pstr "a" <|> pstr "the" <!> "article"
+
+let is_valid(c: char)  = is_letter(c) || is_digit(c) 
 
 let pch = psat is_valid
 
-let header = pseq (pmany1 pch) (pstr ";") (fun (a,b) -> Header (stringify a))
-let object = pseq (pmany1 pch) (pstr ";") (fun (a,b) -> Object (stringify a))
-let category = pright pws1 (pseq (pmany1 pch) (pstr ";") (fun (a,b)-> Category (stringify a)))
-let compare =  pright (pws1) (pmany1 pch) |>> (fun a -> Compare (stringify a))
+let pheader = pstr ("How many") |>> (fun (a) -> HowMany (a)) <!> "header"
 
-let question = pseq header (pseq object (pseq category compare  (fun (a,b)-> (a,b))) (fun (a,(b,c)) -> (a,b,c))) (fun (a,(b,c,d)) -> (a,b,c,d))
+let pobj = pright pws1 (pleft (pmany1 pch) paux)|>> (fun a -> Main (stringify a)) <!> "main object"
+
+let pcat = pright pws1 (pstr ("fit in") |>> (fun (a) -> FitIn (a))) <!> "category"
+
+//let pcomp =  pright pws1 (pbetween particle (pstr "?") (pright pws1 (pmany1 pch))) |>> (fun (a) -> Compare (stringify a)) <!> "comparing"
+
+
+let compws = pws1 <!> "pcompws"
+let pcomp = pright compws (pbetween particle (pchar '?') (pright pws1 (pstr "hhhh"))) <!> "Compare"
+
+//let pcomp =  pright pws1 (pbetween particle (pstr "?") (pright pws1 (pmany1 pch))) |>> (fun (a) -> Compare (stringify a)) <!> "comparing"
+
+
+let query = pseq pheader (pseq pobj (pseq pcat pcomp  (fun (a,b)-> (a,b))) (fun (a,(b,c)) -> (a,b,c))) (fun (a,(b,c,d)) -> (a,b,c,d))
+
+//let query1 = pheader |>> (fun (a) -> (a))
 
 /// <summary>Parses an entire question, including EOF.</summary>
 /// <returns>A Question value.</returns>
-let grammar =  pleft question peof
+let grammar =  pleft query peof
 
 let parse s = 
     match (grammar (prepare s)) with
